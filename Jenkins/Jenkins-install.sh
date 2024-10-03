533bd9384f449194bffb5c40e505232683540d12
maharjansumit07 <64695530+maharjansumit07@users.noreply.github.com>
2024-10-03 11:39:29 +0545

    Update Jenkins-install.sh

--- Jenkins/Jenkins-install.sh
+++ Jenkins/Jenkins-install.sh
@@ -15,9 +15,9 @@ sudo apt-get install -y jenkins
 sudo apt update
 sudo apt install -y fontconfig openjdk-17-jre
 java -version
-openjdk version "17.0.8" 2023-07-18
-OpenJDK Runtime Environment (build 17.0.8+7-Debian-1deb12u1)
-OpenJDK 64-Bit Server VM (build 17.0.8+7-Debian-1deb12u1, mixed mode, sharing)
+#openjdk version "17.0.8" 2023-07-18
+#OpenJDK Runtime Environment (build 17.0.8+7-Debian-1deb12u1)
+#OpenJDK 64-Bit Server VM (build 17.0.8+7-Debian-1deb12u1, mixed mode, sharing)
 
 
 # Add Jenkins to sudo users or add to sudo group or manually add Jenkins user on sudoers file at /etc/sudoers with command 'sudo visudo'
